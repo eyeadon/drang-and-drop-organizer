861dcc65e31142307f6d39b607a4955930797539
eyeadon <eyeadon40@gmail.com>
2026-03-31 17:05:36 -0400

    layout fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,7 @@ export default async function Home() {
       <div className="font-sans grid grid-rows-1 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
           <div className="flex flex-col gap-4 items-center  sm:flex-col">
-            <div className="flex flex-col gap-4 items-center  sm:flex-col">
-              <BoardView authorId={1} board={boards[0]} />
-            </div>
+            <BoardView authorId={1} board={boards[0]} />
           </div>
         </main>
       </div>
